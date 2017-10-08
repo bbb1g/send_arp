8fe1d2ac4eb68f597cf7ce78f416f0c5599c06ac
bbbig <bbbig@bbbigui-MacBook-Pro.local>
2017-10-09 06:05:28 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,15 +9,14 @@ void usage(void){
 	printf("./main <victim's ip>\n");
 }
 
+void print_mac(char * mac);
 
 int main(int argc, char * argv[]){
 
 	char * mydev = 0;
-	char my_mac[6] = {0x4c,0x32,0x75,0x95,0x95,0xed};
 	char victim_mac[6] = {};
 	struct libnet_arp_hdr * packet_s = 0;
 	struct libnet_arp_hdr * packet_r = 0;
-	struct in_addr adr;
 
 	if (argc != 2) {
 		usage();
@@ -39,8 +38,8 @@ int main(int argc, char * argv[]){
 	pcap_if_t * alldevs;
 
 	if (pcap_findalldevs(
-		&alldevs,
-		NULL) != 0
+			&alldevs,
+			NULL) != 0
 		)
 	{
 		puts("pcap_findalldevs ERROR!");
@@ -48,23 +47,51 @@ int main(int argc, char * argv[]){
 	}
 
 	char my_ip[20];
+	char my_mac[6];
+	int tmp=0;
 
 	for(pcap_if_t * i = alldevs; i != NULL; i=i->next){
 		if(strcmp(i->name,mydev)!=0)continue;
 
 		for(pcap_addr_t * j=i->addresses; j!=NULL; j=j->next){
-			if(j->addr->sa_family != AF_INET)continue;
-			strncpy(
-			my_ip,
-			inet_ntoa(((struct sockaddr_in*)j->addr)->sin_addr),
-			18
-			);
-			break;
+			if ( (j->addr->sa_family != AF_INET) && (j->addr->sa_family != AF_LINK))
+				continue;
+
+			if (j->addr->sa_family == AF_INET){
+				strncpy(
+					my_ip,
+					inet_ntoa(((struct sockaddr_in*)j->addr)->sin_addr),
+					18);
+				tmp++;
+			}else{
+				char * mac_addr = (char *)j->addr->sa_data;
+				mac_addr += 9;
+				memcpy(my_mac, mac_addr, 6);
+				tmp++;
+			}
+			if (tmp==2)break;
 		}
 		break;
 	}
 
-	printf("My IP : %s\n",my_ip);
-	//get_victim_mac(victim_ip, victim_mac);
+	pcap_freealldevs(alldevs);
 
+	printf("My IP : %s\n",my_ip);
+	printf("My Mac : ");
+	print_mac(my_mac);
+
+	/*
+	get_victim_mac(
+		victim_ip,
+		victim_mac
+		my_ip
+		my_mac);
+	*/
+
+}
+
+void print_mac(char * mac){
+	printf("%hhx:%hhx:%hhx:%hhx:%hhx:%hhx\n",
+		mac[0],mac[1],mac[2],
+		mac[3],mac[4],mac[5]);
 }
