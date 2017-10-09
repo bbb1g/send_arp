d3a17f0ed415b394dc2aeaab503e8d9d33cd6bfb
bbbig <bbbig@bbbigui-MacBook-Pro.local>
2017-10-10 01:35:24 +0900

    UPDATE main.cpp

--- main.cpp
+++ main.cpp
@@ -2,21 +2,30 @@
 #include <libnet.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define PACKET_SIZE 42
+
+struct tmp_ip{
+	uint8_t sender_mac[6];
+	uint8_t sender_ip[4];
+	uint8_t target_mac[6];
+	uint8_t target_ip[4];
+};
 
 void usage(void){
 	printf("./main <victim's ip>\n");
 }
 
 void print_mac(char * mac);
+void get_victim_mac(char * mydev, 
+	char * victim_ip,char * victim_mac,
+	char * my_ip, char * my_mac);
 
 int main(int argc, char * argv[]){
 
 	char * mydev = 0;
 	char victim_mac[6] = {};
-	struct libnet_arp_hdr * packet_s = 0;
-	struct libnet_arp_hdr * packet_r = 0;
 
 	if (argc != 2) {
 		usage();
@@ -80,16 +89,63 @@ int main(int argc, char * argv[]){
 	printf("My Mac : ");
 	print_mac(my_mac);
 
-	/*
+	
 	get_victim_mac(
+		mydev,
 		victim_ip,
-		victim_mac
-		my_ip
+		victim_mac,
+		my_ip,
 		my_mac);
-	*/
+	
 
 }
+void get_victim_mac(char * mydev, 
+	char * victim_ip,char * victim_mac,
+	char * my_ip, char * my_mac){
 
+	struct libnet_ethernet_hdr * eth_hdr = 0;
+	pcap_t * handler;
+	char packet_s[PACKET_SIZE+1]={};
+	struct libnet_arp_hdr * arp_hdr;
+
+
+	if((handler = pcap_open_live(mydev,1000,1,1000,NULL))==NULL){
+		puts("pcap_open_live ERROR!");
+		exit(1);
+	}
+
+	eth_hdr = (struct libnet_ethernet_hdr *)packet_s;
+
+	memset(eth_hdr->ether_dhost,'\xff',6);
+	memcpy(eth_hdr->ether_shost,my_mac,6);
+	eth_hdr->ether_type = htons(ETHERTYPE_ARP);
+
+	arp_hdr = (libnet_arp_hdr *)((char *)eth_hdr + 
+		sizeof(struct libnet_ethernet_hdr));
+
+	arp_hdr->ar_hrd = htons(ARPHRD_ETHER);
+	arp_hdr->ar_pro = htons(0x0800); //ipv4
+	arp_hdr->ar_hln = 6;
+	arp_hdr->ar_pln = 4;
+	arp_hdr->ar_op = htons(ARPOP_REQUEST);
+
+	struct tmp_ip *ip_info = (struct tmp_ip *)((char *)arp_hdr + 
+		sizeof(struct libnet_arp_hdr));
+
+	memcpy(ip_info->sender_mac,my_mac,6);
+	inet_pton(AF_INET, my_ip, &ip_info->sender_ip);
+	memcpy(ip_info->target_mac,victim_mac,6);
+	inet_pton(AF_INET, victim_ip,&ip_info->target_ip);
+
+	write(1,packet_s,42);
+	if(pcap_sendpacket(handler,(const u_char *)packet_s,42)==-1){
+		puts("pcap_sendpacket Error!");
+		exit(1);
+	}
+	printf("Packet sent\n");
+
+
+}
 void print_mac(char * mac){
 	printf("%hhx:%hhx:%hhx:%hhx:%hhx:%hhx\n",
 		mac[0],mac[1],mac[2],
